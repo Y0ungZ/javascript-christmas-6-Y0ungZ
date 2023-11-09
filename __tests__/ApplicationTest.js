5accf368aa565f37fd5de4a3774076c78a2e0b9a
at8in <at8in@naver.com>
2023-11-10 07:11:14 +0900

    style: 포매팅 적용

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,8 +1,8 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
-import { EOL as LINE_SEPARATOR } from "os";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { EOL as LINE_SEPARATOR } from 'os';
+import App from '../src/App.js';
 
-const mockQuestions = (inputs) => {
+const mockQuestions = inputs => {
   MissionUtils.Console.readLineAsync = jest.fn();
 
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
@@ -13,27 +13,27 @@ const mockQuestions = (inputs) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
 
   return logSpy;
 };
 
-const getOutput = (logSpy) => {
+const getOutput = logSpy => {
   return [...logSpy.mock.calls].join(LINE_SEPARATOR);
 };
 
 const expectLogContains = (received, expectedLogs) => {
-  expectedLogs.forEach((log) => {
+  expectedLogs.forEach(log => {
     expect(received).toContain(log);
   });
 };
 
-describe("기능 테스트", () => {
-  test("모든 타이틀 출력", async () => {
+describe('기능 테스트', () => {
+  test('모든 타이틀 출력', async () => {
     // given
     const logSpy = getLogSpy();
-    mockQuestions(["3", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"]);
+    mockQuestions(['3', '티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1']);
 
     // when
     const app = new App();
@@ -41,62 +41,68 @@ describe("기능 테스트", () => {
 
     // then
     const expected = [
-      "<주문 메뉴>",
-      "<할인 전 총주문 금액>",
-      "<증정 메뉴>",
-      "<혜택 내역>",
-      "<총혜택 금액>",
-      "<할인 후 예상 결제 금액>",
-      "<12월 이벤트 배지>",
+      '<주문 메뉴>',
+      '<할인 전 총주문 금액>',
+      '<증정 메뉴>',
+      '<혜택 내역>',
+      '<총혜택 금액>',
+      '<할인 후 예상 결제 금액>',
+      '<12월 이벤트 배지>',
     ];
 
     expectLogContains(getOutput(logSpy), expected);
   });
 
-  test("혜택 내역 타이틀과 없음 출력", async () => {
+  test('혜택 내역 타이틀과 없음 출력', async () => {
     // given
     const logSpy = getLogSpy();
-    mockQuestions(["26", "타파스-1,제로콜라-1"]);
+    mockQuestions(['26', '타파스-1,제로콜라-1']);
 
     // when
     const app = new App();
     await app.run();
 
     // then
-    const expected = ["<혜택 내역>" + LINE_SEPARATOR + "없음"];
+    const expected = [`<혜택 내역>${LINE_SEPARATOR}없음`];
 
     expectLogContains(getOutput(logSpy), expected);
   });
 });
 
-describe("예외 테스트", () => {
-  test("날짜 예외 테스트", async () => {
+describe('예외 테스트', () => {
+  test('날짜 예외 테스트', async () => {
     // given
-    const INVALID_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    const INPUTS_TO_END = ["1", "해산물파스타-2"];
+    const INVALID_DATE_MESSAGE =
+      '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.';
+    const INPUTS_TO_END = ['1', '해산물파스타-2'];
     const logSpy = getLogSpy();
-    mockQuestions(["a", ...INPUTS_TO_END]);
+    mockQuestions(['a', ...INPUTS_TO_END]);
 
     // when
     const app = new App();
     await app.run();
 
     // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(INVALID_DATE_MESSAGE));
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(INVALID_DATE_MESSAGE),
+    );
   });
 
-  test("주문 예외 테스트", async () => {
+  test('주문 예외 테스트', async () => {
     // given
-    const INVALID_ORDER_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
-    const INPUTS_TO_END = ["해산물파스타-2"];
+    const INVALID_ORDER_MESSAGE =
+      '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.';
+    const INPUTS_TO_END = ['해산물파스타-2'];
     const logSpy = getLogSpy();
-    mockQuestions(["3", "제로콜라-a", ...INPUTS_TO_END]);
+    mockQuestions(['3', '제로콜라-a', ...INPUTS_TO_END]);
 
     // when
     const app = new App();
     await app.run();
 
     // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(INVALID_ORDER_MESSAGE));
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(INVALID_ORDER_MESSAGE),
+    );
   });
 });
