6ed243b91c6b33dfdb6d5ccf30c8b78c8d64270e
at8in <at8in@naver.com>
2023-11-12 06:06:47 +0900

    feat: 특별 할인 메서드

--- src/constants/discounts.js
+++ src/constants/discounts.js
@@ -1,8 +1,11 @@
+const special = Object.freeze([3, 10, 17, 24, 25, 31]);
+
 export const DAY_DISCOUNT = Object.freeze({
   basePrice: 1000,
   increasePrice: 100,
   startDay: 1,
   endDay: 25,
+  special,
 });
 
 export const MENU_DISCOUNT = 2023;
--- src/domain/discount-event/DayDiscount.js
+++ src/domain/discount-event/DayDiscount.js
@@ -12,6 +12,13 @@ class DayDiscount {
     }
     return DAY_DISCOUNT.basePrice + DAY_DISCOUNT.increasePrice * (this.day - 1);
   }
+
+  calculateSpecialBenefitPrice() {
+    if (DAY_DISCOUNT.special.includes(this.day)) {
+      return DAY_DISCOUNT.basePrice;
+    }
+    return 0;
+  }
 }
 
 export default DayDiscount;
