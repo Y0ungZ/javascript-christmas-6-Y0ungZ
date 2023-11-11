837ac7af5e6d1be71c9db2d8f7905a07c4f58e9c
at8in <at8in@naver.com>
2023-11-12 05:49:46 +0900

    refactor: given-when-then 주석 추가

--- __tests__/DayDiscountTest.js
+++ __tests__/DayDiscountTest.js
@@ -1,17 +1,19 @@
 import DayDiscount from '../src/domain/discount-event/DayDiscount';
 
 describe('디데이 할인 테스트', () => {
+  // given
   const increaseDays = Array.from({ length: 25 }, (_, idx) => [
     idx + 1,
     1000 + 100 * idx,
   ]);
-
   const notApplyDays = Array.from({ length: 6 }, (_, idx) => [26 + idx, 0]);
 
   test.each([...increaseDays, ...notApplyDays])(
     '%s일의 디데이 할인 적용 금액은 %s이다.',
     (day, benefitPrice) => {
+      // when
       const dayDiscount = new DayDiscount(day);
+      // then
       expect(dayDiscount.calculateBenefitPrice()).toBe(benefitPrice);
     },
   );
