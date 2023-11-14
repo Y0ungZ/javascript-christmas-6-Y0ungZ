6cd8dff756838e1fda32bede114dc64c8d90c52c
at8in <at8in@naver.com>
2023-11-15 02:36:04 +0900

    refactor: 기존 InputView 수정

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -3,3 +3,8 @@ export const ERROR_MESSAGE = Object.freeze({
   invalidDay: '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
   invalidOrder: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
 });
+
+export const INPUT_MESSAGE = Object.freeze({
+  day: '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+  menu: '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+});
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,13 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
+import { INPUT_MESSAGE } from '../constants/messages.js';
 
 const InputView = {
-  async readDate() {
-    const input = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
-    );
-    // ...
+  async readDay() {
+    const input = await Console.readLineAsync(INPUT_MESSAGE.day);
   },
-  // ...
 };
 
 export default InputView;
