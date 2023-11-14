c77ff1dcdbce7ff60b3701481c53df6aa595392a
at8in <at8in@naver.com>
2023-11-15 01:35:49 +0900

    feat: 뱃지 이벤트 클래스

---
+++ src/constants/badge.js
@@ -0,0 +1,10 @@
+const types = Object.freeze(['별', '트리', '산타']);
+
+export const BADGE_STANDARD = Object.freeze({
+  basePrice: 5000,
+  types,
+  별: 1,
+  트리: 2,
+  산타: 4,
+  defaultType: '없음',
+});
---
+++ src/domain/gift-event/BadgeEvent.js
@@ -0,0 +1,22 @@
+import { BADGE_STANDARD } from '../../constants/badge';
+
+class BadgeEvent {
+  constructor(totalBenefitPrice) {
+    this.totalBenefitPrice = totalBenefitPrice;
+  }
+
+  calculateBadgeType() {
+    let result = BADGE_STANDARD.defaultType;
+    const quot = Math.floor(this.totalBenefitPrice / BADGE_STANDARD.basePrice);
+
+    BADGE_STANDARD.types.forEach(type => {
+      if (quot >= BADGE_STANDARD[type]) {
+        result = type;
+      }
+    });
+
+    return result;
+  }
+}
+
+export default BadgeEvent;
