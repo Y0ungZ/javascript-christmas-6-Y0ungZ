40b38f502274619178ff0a46be9970a8e04510c3
at8in <at8in@naver.com>
2023-11-15 01:50:46 +0900

    feat: 커스텀 에러 추가

---
+++ src/constants/messages.js
@@ -0,0 +1,5 @@
+export const ERROR_PRIFIX = '[ERROR] ';
+export const ERROR_MESSAGE = Object.freeze({
+  invalidDay: '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+  invalidOrder: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
+});
