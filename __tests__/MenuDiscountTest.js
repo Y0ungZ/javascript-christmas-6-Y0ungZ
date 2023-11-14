a1ed5017e37d692405a88411fb978a2b03c3680c
at8in <at8in@naver.com>
2023-11-15 05:02:27 +0900

    test: 메뉴 할인 테스트 리팩토링

--- __tests__/MenuDiscountTest.js
+++ __tests__/MenuDiscountTest.js
@@ -1,20 +1,21 @@
+import { WEEKDAY_TEXT, WEEKEND_TEXT } from '../src/constants/discounts.js';
 import MenuDiscount from '../src/domain/discount-event/MenuDiscount.js';
 
 describe('메뉴 할인 테스트', () => {
   // given
   const dessertInputs = [
-    ['시저샐러드,티본스테이크,바비큐립', 0],
-    ['해산물파스타', 0],
-    ['초코케이크,초코케이크', 4046],
-    ['초코케이크,크리스마스파스타', 2023],
-    ['아이스크림,초코케이크', 4046],
+    [['시저샐러드-1', '티본스테이크-1', '바비큐립-1'], 0],
+    [['해산물파스타-1'], 0],
+    [['초코케이크-2'], 4046],
+    [['초코케이크-1', '크리스마스파스타-1'], 2023],
+    [['아이스크림-2', '초코케이크-1'], 6069],
   ];
 
   test.each([...dessertInputs])(
-    '디저트 메뉴 1개당 2,023원이 할인된다.',
+    '평일에는 디저트 메뉴 1개당 2,023원이 할인된다.',
     (input, benefitPrice) => {
       // when
-      const menuDiscount = new MenuDiscount('디저트', input.split(','));
+      const menuDiscount = new MenuDiscount(WEEKDAY_TEXT, input);
       // then
       expect(menuDiscount.calculateBenefitPrice()).toBe(benefitPrice);
     },
@@ -22,18 +23,18 @@ describe('메뉴 할인 테스트', () => {
 
   // given
   const mainInputs = [
-    ['시저샐러드,티본스테이크,바비큐립', 4046],
-    ['해산물파스타', 2023],
-    ['해산물파스타,해산물파스타', 4046],
-    ['초코케이크,크리스마스파스타', 2023],
-    ['아이스크림,초코케이크', 0],
+    [['시저샐러드-1', '티본스테이크-1', '바비큐립-1'], 4046],
+    [['해산물파스타-1'], 2023],
+    [['해산물파스타-2'], 4046],
+    [['초코케이크-1', '크리스마스파스타-1'], 2023],
+    [['아이스크림-1', '초코케이크-1'], 0],
   ];
 
   test.each([...mainInputs])(
-    '메인 메뉴 1개당 2,023원이 할인된다.',
+    '주말에는 메인 메뉴 1개당 2,023원이 할인된다.',
     (input, benefitPrice) => {
       // when
-      const menuDiscount = new MenuDiscount('메인', input.split(','));
+      const menuDiscount = new MenuDiscount(WEEKEND_TEXT, input);
       // then
       expect(menuDiscount.calculateBenefitPrice()).toBe(benefitPrice);
     },
